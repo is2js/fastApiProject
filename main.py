bf9265b1e5da8e3ff31d4812bc6c2fca29ad5e2e
is2js <tingstyle1@gmail.com>
2023-09-21 13:35:30 +0900

    feat: uvicorn.run을 event_loop안에서 돌리기

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import asyncio
 import os
 
+# if __name__ == "__mp_main__":
+#     """Option 1: Skip section for multiprocess spawning
+#     This section will be skipped when running in multiprocessing mode"""
+#     pass
+#
+# elif __name__ == '__main__':
 if __name__ == "__mp_main__":
-    """Option 1: Skip section for multiprocess spawning
-    This section will be skipped when running in multiprocessing mode"""
-    pass
-
-elif __name__ == '__main__':
     if os.getenv('API_ENV') != 'test':
         os.environ["API_ENV"] = "local"
 
@@ -18,7 +20,10 @@ elif __name__ == '__main__':
     app = create_app(config)
 
     # uvicorn.run("app.main:app", port=config.PORT, reload=config.PROJ_RELOAD)
-    uvicorn.run("main:app", port=config.PORT, reload=config.PROJ_RELOAD)
+    event_loop = asyncio.get_event_loop()
+    event_loop.run_until_complete(
+        uvicorn.run("main:app", port=config.PORT, reload=config.PROJ_RELOAD)
+    )
 
 else:
     # docker 용
